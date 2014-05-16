d2465935cc9bd6a89e4f4ccabdb97e0bf43fcb75
gondars <g0ndars@yahoo.com>
2014-05-16 21:34:42 +0300

    Update InputHandler.java

--- Team-Project-Pong/src/InputHandler.java
+++ Team-Project-Pong/src/InputHandler.java
@@ -11,20 +11,21 @@ public class InputHandler implements KeyListener {
 	
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-		
-		if (keyCode == e.VK_UP) {
+		//giving players movement
+		if (keyCode == KeyEvent.VK_UP) {
 			Game.player2.goingDown = false;
-			Game.player2.goingUp = true;			
+			Game.player2.goingUp = true;	
+			
 		}
-		if (keyCode == e.VK_DOWN) {
+		if (keyCode == KeyEvent.VK_DOWN) {
 			Game.player2.goingUp = false;
 			Game.player2.goingDown=true;			
 		}
-		if (keyCode == e.VK_W) {
+		if (keyCode == KeyEvent.VK_W) {
 			Game.player.goingDown = false;
 			Game.player.goingUp = true;
 		}
-		if (keyCode == e.VK_S) {
+		if (keyCode == KeyEvent.VK_S) {
 			Game.player.goingUp = false;
 			Game.player.goingDown = true;
 		}
@@ -35,26 +36,26 @@ public class InputHandler implements KeyListener {
 	public void keyReleased(KeyEvent e) {
 		int keyCode = e.getKeyCode();
 		
-		//player 2 controls
-		if (keyCode == e.VK_UP) {
+		//improving players movement for released key
+		if (keyCode == KeyEvent.VK_UP) {
 			Game.player2.goingDown = false;
 			Game.player2.goingUp = false;
 		}
-		if (keyCode == e.VK_DOWN) {
+		if (keyCode == KeyEvent.VK_DOWN) {
 			Game.player2.goingDown = false;
 			Game.player2.goingUp = false;
 		}
-		//player 1 controls
-		if (keyCode == e.VK_W) {
+		
+		if (keyCode == KeyEvent.VK_W) {
 			Game.player.goingDown = false;
 			Game.player.goingUp = false;
 		}
-		if (keyCode == e.VK_S) {
+		if (keyCode == KeyEvent.VK_S) {
 			Game.player.goingDown = false;
 			Game.player.goingUp = false;
 		}
-		//exit button ingame
-		if (keyCode == e.VK_ESCAPE) {
+		//exit button in the game
+		if (keyCode == KeyEvent.VK_ESCAPE) {
 			System.exit(0);
 		}
 		
